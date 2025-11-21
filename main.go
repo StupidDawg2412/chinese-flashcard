a6499c2272dbdbaf328b56d42910c227d0fce956
Seksan Nonmee <seksann@rabbit.co.th>
2025-11-21 14:47:20 +0700

    init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func consec(arr []int, k int) int {
+	// start, end, cnt := 0, 0, 0
+	// for cnt
+	return 1
+}
+
+func main() {
+	arr := []int{1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1}
+	k := 2
+
+	result := consec(arr, k)
+
+	fmt.Println(result)
+}
